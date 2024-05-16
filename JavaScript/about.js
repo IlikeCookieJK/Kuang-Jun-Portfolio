76cc98f5a44628c1b5ef10ed64fd60e6524f9da7
IlikeCookieJK <jun_hong.kuang23@student.cart.org>
2024-05-15 23:24:00 -0700

    About Hobbies Carousel

---
+++ JavaScript/about.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let nextBttn = document.getElementById("next");
+    let previewBttn = document.getElementById("preview");
+    let carouselItem = document.getElementById("item1")
+    nextBttn.addEventListener("click", () => {
+        carouselItem.classList.add("hidden");
+        item = Array.from(carouselItem.id);
+        number = item[item.length - 1];
+        console.log(parseInt(number))
+        if (parseInt(number) === 5) {
+            item.pop();
+            item.push(1);
+            itemID = item.join(``);
+            changeCarouselItem(itemID);
+        } else {
+            item.pop();
+            item.push(parseInt(number) + 1);
+            itemID = item.join(``);
+            changeCarouselItem(itemID);
+        }
+    });
+    previewBttn.addEventListener("click", () => {
+        carouselItem.classList.add("hidden");
+        item = Array.from(carouselItem.id);
+        number = item[item.length - 1];
+        newNumber = parseInt(number) - 1;
+        if (newNumber === 0) {
+            item.pop();
+            item.push(5);
+            itemID = item.join(``);
+            changeCarouselItem(itemID);
+        } else {
+            item.pop();
+            item.push(parseInt(number) - 1);
+            itemID = item.join(``);
+            changeCarouselItem(itemID);
+        }
+    });
+
+    function changeCarouselItem(id) {
+        carouselItem = document.getElementById(`${id}`);
+        carouselItem.classList.remove("hidden");
+        carouselItem.classList.add("appear");
+    }
+});
